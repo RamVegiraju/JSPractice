4b77169cd56712aaf701fba53f227698738ccde9
RamVegiraju <rav3sa@virginia.edu>
2020-12-03 22:35:27 -0500

    Fundamentals section added

---
+++ FundamentalsJS/scripts.js
@@ -0,0 +1,32 @@
+//strict mode helps catch errors
+'use strict';
+
+
+let hasDriversLicense = false;
+const passTest = true;
+
+
+if (passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive :D');
+
+//reserving words for future JS features
+/*
+None of these below work
+const interface = 'Audio'; 
+const private = 534;
+const if = 23;
+*/
+
+
+//FUNCTIONS Practice
+
+//sample function
+function logger() {
+    console.log('My name is Ram');
+}
+
+//invocation
+logger();
+logger();
+
+
---
+++ OOP/index.js
@@ -0,0 +1,19 @@
+/**
+ * OOP Intro with JS
+ */
+
+//arrow functions do not work with constructor
+
+ const Person = function(firstName, birthYear) {
+     console.log(this);
+ }
+
+
+ new Person('Jonas', 1991);
+
+
+
+
+
+
+
