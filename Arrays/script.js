8290c6b8ce8cb9d141c1fd2ec409d4f36e8b4c03
RamVegiraju <rav3sa@virginia.edu>
2020-12-25 01:54:09 -0500

    Array Methods added

--- Arrays/script.js
+++ Arrays/script.js
@@ -65,12 +65,160 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+/*
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+*/
+
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+///slice method
+//start extracting at 2/c
+//does not mutate the array returns a new array
+console.log(arr.slice(2));
+//end param
+console.log(arr.slice(2,4));
+//end of array
+console.log(arr.slice(-2));
+//creating a shallow copy
+console.log(arr.slice());
+
+
+///SPLICE METHOD
+//actually mutates the array, does not create a new array
+//console.log(arr.splice(2));
+arr.splice(-1);
+arr.splice(1, 2);
+console.log(arr); //original array except last element
+
+
+// REVERSE METHOD
+//does mutate original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'j'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+
+// CONCAT Method
+// concatenate two arrays
+// does not mutate the original arrays
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr,...arr2]);
+
+
+// JOIN Method
+//can join elements of an array
+//will not mutate the originl array
+console.log(letters.join('-'));
+
+*/
+
+
+///Looping Arrays: forEach
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/*
+//iterating with a for-of loop for comparision
+for (const movement of movements) {
+  if(movement > 0) {
+    console.log(`You deposited ${movement}`);
+  }else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+
+console.log('---------FOROF with COUNTER----------------');
+
+
+
+
+//what if we wanted a counter?
+//first element i is the index and second is the current element
+for (const [i, movement] of movements.entries()) {
+  if(movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  }else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+
+
+console.log('---------FOREACH----------------');
+
+//https://www.w3schools.com/js/js_callback.asp
+//for each requires a callback function
+
+movements.forEach(function(movement) {
+  if(movement > 0) {
+    console.log(`You deposited ${movement}`);
+  }else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+
+console.log('---------FOREACH WITH COUNTER----------------');
+
+//FOREACH with a counter
+//the order of params: current element, current index, and entire array we are looping over
+movements.forEach(function(movement, i, array) {
+  if(movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  }else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+
+//When to use both
+//Cannot break out of a ForEach Loop, will go over the entire array
+//Use For-Of if you need to break out of a loop
+
+*/
+
+
+
+
+///ForEach with Maps and Sets
+
+///ForEach with Map
+
+//first item is the key, second item is the value
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//first param is value
+//second param is key
+//third param is entire map
+currencies.forEach(function(value, key, map){
+  console.log(`${key}: ${value}`);
+})
 
-/////////////////////////////////////////////////
+
+
+///ForEach with Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+//same params as map
+//set does not have keys or indexes however
+//key does not really make sense as a param
+//use underscore for a variable which is unncecessary
+currenciesUnique.forEach(function(value, _, map){
+  console.log(`${value}: ${value}`);
+})
--- DataStructures/script.js
+++ DataStructures/script.js
@@ -7,9 +7,6 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  order: function(starterIndex, mainIndex) {
-      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
   openingHours: {
     thu: {
       open: 12,
@@ -24,11 +21,63 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  //passes in an object, destructure to 4 variables
+  //can set default values as well when passing in an object
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+    console.log(`Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
 
+restaurant.orderDelivery({
+  time: '22: 30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2
+});
 
+
+///Destructuring Objects
+//Use {} for destructuring
+//remeber order of elements does not matter in objects, so do not need to skip like we do with arrays
+
+//creates three brand new variables 
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+//outputs exact same as before
+//if you want to change the variable names from the name in object properties
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//no property called menu, but can set a default value
+//default values
+const { menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+let a = 111;
+let b = 999;
+
+//want a to become 23 and b to become 7
+const obj = {a: 23, b: 7, c:14};
+
+({a,b} = obj);
+console.log(a,b);
+
+// nested objects
+const {fri: {open: o, close: c}} = openingHours;
+console.log(o, c);
+
+///Destructuring Arrays
 //destructuring is unpacking a data structure into small pieces
-//if you wanted to without destructuring
+//if you wanted to without 
+
+/*
 const arr = [2,3,4];
 const a = arr[0];
 const b = arr[1];
@@ -36,27 +85,27 @@ const c = arr[2];
 
 //destructuring way
 const [x, y, z] = arr;
-console.log(x,y,z);
+//console.log(x,y,z);
 
 
 //with our restaurant example
 let [first, second] = restaurant.categories;
-console.log(first, second);
+//console.log(first, second);
 
 //say we wanted just the first and third elements
 //just leave an empty whole for element we don't want
 let [main, ,secondary] = restaurant.categories;
-console.log(main, secondary);
+//console.log(main, secondary);
 
 //if we want to reassign values
 //switching variables
 [main, secondary] = [secondary, main];
-console.log(main, secondary); //now returns vegetarian italian instead of italian vegetarian
+//console.log(main, secondary); //now returns vegetarian italian instead of italian vegetarian
 
 //want element 2 and 0 from starter and main menu
-console.log(restaurant.order(2,0));
+//console.log(restaurant.order(2,0));
 const [starter, mainCourse] = restaurant.order(2,0);
-console.log(starter, mainCourse);
+//console.log(starter, mainCourse);
 
 
 //what about nested arrays
@@ -64,4 +113,9 @@ const nested = [2, 4, [5,6]];
 //const [i, ,j] = nested;
 //console.log(i,j);
 const [i, ,[j, k]] = nested;
-console.log(i, j, k);
+//console.log(i, j, k);
+
+*/
+
+
+
